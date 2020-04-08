9288ad5224a08b5de8f9b4498ea6268d578b1b8b
revsic <revsic99@gmail.com>
2020-04-09 01:29:38 +0900

    test_userapp: user application for test purpose

---
+++ xv6-public/test_userapp.c
@@ -0,0 +1,28 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char* argv[])
+{
+    int pid = fork();
+    if (pid < 0) {
+        printf(1, "fork failure\n");
+        exit();
+    }
+
+    for (int i = 0; i < 20; ++i) {
+        if (pid == 0) {
+            printf(1, "Child\n");
+            yield();
+        } else {
+            printf(1, "Parent\n");
+            yield();
+        }
+    }
+
+    if (pid > 0) {
+        wait();
+    }
+    return 0;
+}
