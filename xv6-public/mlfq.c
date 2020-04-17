35c7b414b5207f2c4918cd296f4cf7d8b873dbff
revsic <revsic99@gmail.com>
2020-04-17 17:57:12 +0900

    mlfq: Add method mlfq_scheduler

--- xv6-public/mlfq.c
+++ xv6-public/mlfq.c
@@ -1,5 +1,5 @@
-int
-mlfq_init(struct mlfq* this, int num_queue, unsigned long long* rr, unsigned long long* expire, unsigned long long boost)
+void
+mlfq_init(struct mlfq* this, int num_queue, uint* rr, uint* expire, uint boost)
 {
   int i;
   this->num_queue = num_queue;
@@ -15,8 +15,8 @@ mlfq_init(struct mlfq* this, int num_queue, unsigned long long* rr, unsigned lon
 int
 mlfq_default(struct mlfq* this)
 {
-  unsigned long long rr[] = { 1, 2, 4 };
-  unsigned long long expire[] = { 5, 10, ~(1 << 31) };
+  uint rr[] = { 1, 2, 4 };
+  uint expire[] = { 5, 10, ~(1 << 31) };
   return mlfq_init(this, 3, rr, expire, 100);
 }
 
@@ -24,17 +24,18 @@ int
 mlfq_append(struct mlfq* this, struct proc* p)
 {
   p->elapsed = 0;
-  if (this->sizes[0] < NPROC) {
-    this->queue[0][this->sizes[0]] = p;
-    
-    p->quelevel = 0;
-    p->queindex = this->sizes[0];
-    p->elapsed = 0;
-
-    this->sizes[0]++;
-    return MLFQ_SUCCESS;
+  if (this->sizes[0] >= NPROC) {
+    return MLFQ_FULL_QUEUE;
   }
-  return MLFQ_FULL_QUEUE;
+
+  this->queue[0][this->sizes[0]] = p;
+  
+  p->sched.quelevel = 0;
+  p->sched.queindex = this->sizes[0];
+  p->sched.elapsed = 0;
+
+  this->sizes[0]++;
+  return MLFQ_SUCCESS;
 }
 
 struct proc*
@@ -51,6 +52,12 @@ mlfq_top(struct mlfq* this)
 
 int
 mlfq_update(struct mlfq* this, struct proc* p, int done)
+{
+  
+}
+
+void
+mlfq_scheduler(struct mlfq* this, struct spinlock* lock)
 {
   
 }
