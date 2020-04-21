047059947d5126da7b460004cad07c644ba682f5
revsic <revsic99@gmail.com>
2020-04-21 18:32:38 +0900

    defs: Add getlev

--- xv6-public/defs.h
+++ xv6-public/defs.h
@@ -122,6 +122,7 @@ void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
 void            yield(void);
+int             getlev(void);
 
 // swtch.S
 void            swtch(struct context**, struct context*);
