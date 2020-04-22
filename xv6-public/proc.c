b8de872f695e85a07d17e6eef2bbfd44b53de2e4
revsic <revsic99@gmail.com>
2020-04-23 03:02:37 +0900

    proc: Remove unused

--- xv6-public/proc.c
+++ xv6-public/proc.c
@@ -329,38 +329,6 @@ void
 scheduler(void)
 {
   mlfq_scheduler(&mlfq, &ptable.lock);
-
-  // struct proc *p;
-  // struct cpu *c = mycpu();
-  // c->proc = 0;
-  
-  // for(;;){
-  //   // Enable interrupts on this processor.
-  //   sti();
-
-  //   // Loop over process table looking for process to run.
-  //   acquire(&ptable.lock);
-  //   for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-  //     if(p->state != RUNNABLE)
-  //       continue;
-
-  //     // Switch to chosen process.  It is the process's job
-  //     // to release ptable.lock and then reacquire it
-  //     // before jumping back to us.
-  //     c->proc = p;
-  //     switchuvm(p);
-  //     p->state = RUNNING;
-
-  //     swtch(&(c->scheduler), p->context);
-  //     switchkvm();
-
-  //     // Process is done running for now.
-  //     // It should have changed its p->state before coming back.
-  //     c->proc = 0;
-  //   }
-  //   release(&ptable.lock);
-
-  // }
 }
 
 // Enter scheduler.  Must hold only ptable.lock
@@ -551,8 +519,9 @@ getlev(void)
   return p->mlfq.level;
 }
 
-void
+int
 set_cpu_share(int percent)
 {
-
+  // return mlfq_cpu_share(&mlfq, myproc(), percent);
+  return 1;
 }
