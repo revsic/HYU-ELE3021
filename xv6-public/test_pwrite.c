64a0b4ea49c0cc14947756a78f1c940dd3288639
revsic <revsic99@gmail.com>
2020-06-10 01:40:29 +0900

    test pwrite: Add read tests

--- xv6-public/test_pwrite.c
+++ xv6-public/test_pwrite.c
@@ -7,6 +7,14 @@
 
 #define ASSERT(x, n) ASSERT_(x, n, __LINE__, 0)
 
+int strncmp(const char* a, const char* b, int len) {
+  int i;
+  for (i = 0; i < len; ++i)
+    if (a[i] != b[i])
+      return a[i] - b[i];
+  return 0;
+}
+
 void readtest(const char* filename, const char* answer, int line) {
   char buffer[1024];
   int len = strlen(answer);
@@ -80,10 +88,49 @@ void test_pwrite3() {
   close(fd);
 }
 
+// case1. pread on beginning.
+void test_pread1() {
+  int fd = open("testfile", O_CREATE|O_RDWR);
+  ASSERT(pwrite(fd, "asdfqwerzxcv", 13, 0), 13);
+
+  char buffer[1024];
+  ASSERT(pread(fd, buffer, 4, 0), 4);
+  ASSERT(strncmp(buffer, "asdf", 4), 0);
+  ASSERT(pread(fd, buffer, 5, 3), 5);
+  ASSERT(strncmp(buffer, "fqwer", 5), 0);
+  ASSERT(pread(fd, buffer, 5, 8), 5);
+  ASSERT(strcmp(buffer, "zxcv"), 0);
+
+  printf(1, "test_pread1 done\n");
+  close(fd);
+}
+
+// case2. pread after read.
+void test_pread2() {
+  int fd = open("testfile", O_CREATE|O_RDWR);
+  ASSERT(pwrite(fd, "asdfqwerzxcv", 13, 0), 13);
+
+  char buffer[1024];
+  ASSERT(read(fd, buffer, 4), 4);
+  ASSERT(pread(fd, buffer, 4, 0), 4);
+  ASSERT(strncmp(buffer, "qwer", 4), 0);
+  ASSERT(pread(fd, buffer, 5, 3), 5);
+  ASSERT(strncmp(buffer, "rzxcv", 5), 0);
+
+  ASSERT(pwrite(fd, "1234567", 7, 0), 7);
+  ASSERT(pread(fd, buffer, 9, 0), 9);
+  ASSERT(strcmp(buffer, "1234567v"), 0);
+
+  printf(1, "test_pread2 done\n");
+  close(fd);
+}
+
 int main(int argc, char *argv[]) {
   test_pwrite1();
   test_pwrite2();
   test_pwrite3();
+  test_pread1();
+  test_pread2();
   exit();
   return 0;
 }
