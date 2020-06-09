a4d72984eab5b78e1f551cad696eac455c5345b2
revsic <revsic99@gmail.com>
2020-06-10 01:22:44 +0900

    defs: Add pwrite and pread

--- xv6-public/defs.h
+++ xv6-public/defs.h
@@ -36,6 +36,8 @@ void            fileinit(void);
 int             fileread(struct file*, char*, int n);
 int             filestat(struct file*, struct stat*);
 int             filewrite(struct file*, char*, int n);
+int             filepread(struct file*, char*, int, int);
+int             filepwrite(struct file*, char*, int, int);
 
 // fs.c
 void            readsb(int dev, struct superblock *sb);
